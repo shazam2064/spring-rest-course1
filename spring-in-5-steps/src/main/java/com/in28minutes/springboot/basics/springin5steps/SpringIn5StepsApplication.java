ffee0804558c3cc49f84280cb56b1a5ea984d8d0
gabriel.salomon <gabriel.salomon.2007@gmail.com>
2020-02-16 12:13:56 -0600

    I downloaded a project

---
+++ spring-in-5-steps/src/main/java/com/in28minutes/springboot/basics/springin5steps/SpringIn5StepsApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes.springboot.basics.springin5steps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIn5StepsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIn5StepsApplication.class, args);
+	}
+
+}
---
+++ spring-in-5-steps/src/test/java/com/in28minutes/springboot/basics/springin5steps/SpringIn5StepsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.springboot.basics.springin5steps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIn5StepsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
